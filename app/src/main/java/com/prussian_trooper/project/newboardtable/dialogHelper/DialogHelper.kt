109ef9d03290060a157e16ca18135cac523e3710
Lukoil39 <goshavasilevskiy@mail.ru>
2021-12-15 21:18:40 +0200

    DialogHelper code refactoring

--- app/src/main/java/com/prussian_trooper/project/newboardtable/dialogHelper/DialogHelper.kt
+++ app/src/main/java/com/prussian_trooper/project/newboardtable/dialogHelper/DialogHelper.kt
@@ -1,6 +1,7 @@
 package com.prussian_trooper.project.newboardtable.dialogHelper
 
 import android.app.AlertDialog
+import android.view.View
 import com.prussian_trooper.project.newboardtable.MainActivity
 import com.prussian_trooper.project.newboardtable.R
 import com.prussian_trooper.project.newboardtable.accountHelper.AccountHelper
@@ -14,31 +15,40 @@ class DialogHelper(act: MainActivity) {
          val builder = AlertDialog.Builder(act)
          val rootDialogElement = SignDialogBinding.inflate(act.layoutInflater)
          val view = rootDialogElement.root
-        builder.setView(view)
-         if (index == DialogConst.SIGN_UP_STATE) {
+         builder.setView(view)
+         setDialogState(index, rootDialogElement)
 
-             rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_up)
-             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
-         } else {
-             rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_in)
-             rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_in_action)
-
-         }
         val dialog = builder.create()
 
          rootDialogElement.btSignUpIn.setOnClickListener{
-             dialog.dismiss()
-             if (index == DialogConst.SIGN_UP_STATE) {
-
-                 accHelper.signUpWithEmail(rootDialogElement.edSignEmail.text.toString(),
-                     rootDialogElement.edSignPassword.text.toString())
-             } else {
-                 accHelper.signInWithEmail(rootDialogElement.edSignEmail.text.toString(),
-                     rootDialogElement.edSignPassword.text.toString())
-             }
+             setOnClickSignUpIn(index, rootDialogElement, dialog)
          }
 
          dialog.show()
 
      }
+
+    private fun setOnClickSignUpIn(index: Int, rootDialogElement: SignDialogBinding, dialog: AlertDialog?) {
+        dialog?.dismiss()
+        if (index == DialogConst.SIGN_UP_STATE) {
+
+            accHelper.signUpWithEmail(rootDialogElement.edSignEmail.text.toString(),
+                rootDialogElement.edSignPassword.text.toString())
+        } else {
+            accHelper.signInWithEmail(rootDialogElement.edSignEmail.text.toString(),
+                rootDialogElement.edSignPassword.text.toString())
+        }
+    }
+
+    private fun setDialogState(index: Int, rootDialogElement: SignDialogBinding) {
+        if (index == DialogConst.SIGN_UP_STATE) {
+
+            rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_up)
+            rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_up_action)
+        } else {
+            rootDialogElement.tvSignTitle.text = act.resources.getString(R.string.ac_sign_in)
+            rootDialogElement.btSignUpIn.text = act.resources.getString(R.string.sign_in_action)
+            rootDialogElement.btnForgetP.visibility = View.VISIBLE
+        }
+    }
 }
