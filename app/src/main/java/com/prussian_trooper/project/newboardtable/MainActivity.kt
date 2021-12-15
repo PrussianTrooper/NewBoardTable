e2f9119099e7085ae236fe057f19f1441da1f812
Lukoil39 <goshavasilevskiy@mail.ru>
2021-12-15 14:11:48 +0200

    root element created

--- app/src/main/java/com/prussian_trooper/project/newboardtable/MainActivity.kt
+++ app/src/main/java/com/prussian_trooper/project/newboardtable/MainActivity.kt
@@ -2,10 +2,69 @@ package com.prussian_trooper.project.newboardtable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.navigation.NavigationView
+import com.prussian_trooper.project.newboardtable.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var rootElement: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rootElement = ActivityMainBinding.inflate(layoutInflater)
+        val view = rootElement.root
+        setContentView(view)
+        init()
+
     }
+
+    private fun init(){
+        val toggle = ActionBarDrawerToggle(this, rootElement.drawerLayout, rootElement.mainContent.toolbar, R.string.open, R.string.close)
+        rootElement.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        rootElement.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.id_my_ads -> {
+                /*Проверка работаспособности*/
+                Toast.makeText(this, "Pressed id_my_ads", Toast.LENGTH_LONG).show()
+            }
+            R.id.id_car -> {
+                Toast.makeText(this, "Pressed id_car", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.id_pc -> {
+                Toast.makeText(this, "Pressed id_pc", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.id_smart -> {
+                Toast.makeText(this, "Pressed id_smart", Toast.LENGTH_LONG).show()
+            }
+
+            R.id.id_dm -> {
+                Toast.makeText(this, "Pressed id_dm", Toast.LENGTH_LONG).show()
+            }
+            /*Вход для регистрации*/
+            R.id.id_sign_up -> {
+            }
+            /*Вход для входа :) */
+            R.id.id_sing_in -> {
+            }
+
+            R.id.id_sign_out -> {
+
+            }
+        }
+        return true
+    }
+
+
+
+
 }
